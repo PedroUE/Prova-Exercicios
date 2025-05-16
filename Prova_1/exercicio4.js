cdee6057e7ecd5e1b8004ebe01859ba58f4e2b45
Pedro U <pedroescapalete@gmail.com>
2025-05-16 08:31:03 -0300

    Prova 1

---
+++ Prova_1/exercicio4.js
@@ -0,0 +1,8 @@
+let numeros = [5, 2, 3, 2, 4];
+let soma = 0;
+
+for (let i = 0; i < numeros.length; i++){
+    console.log('Soma:', soma);
+    console.log('Numero', numeros [i]);
+    soma = soma + numeros[i];
+}
